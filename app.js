620312a92905fe3239d84f151915595606445fc5
DESKTOP-89N3UKL\danch <dancheskus@gmail.com>
2018-07-14 23:40:09 +0300

    Все работает 14.07.2018

--- app.js
+++ app.js
@@ -4,26 +4,38 @@ const moment = require('moment');
 
 const base_url = 'https://ticket.luxexpress.eu/ru';
 const routesFromAllPages = [];
-const query = [{ Legs: [] }];
 const allQueries = [];
 
 /////////////////// Запросы на сервер //////////////////////////
 const concurrency = 5;
-const legsPerQuery = 50;
+const legsPerQuery = 130;
 
 /////////////////// Настройка дат //////////////////////////
-const start_date = moment([2018, 8, 10]);
-const end_date = moment([2018, 8, 14]);
-// const start_date = moment();
-// const end_date = moment()
-//   .add(6, 'months')
-//   .subtract(3, 'd');
+const start_date = moment();
+let end_date = moment().add(6, 'months');
+
+if (
+  // Ограничиваем 6 месяцами
+  end_date.format('MM-DD-YYYY') >=
+  moment()
+    .add(6, 'months')
+    .subtract(3, 'd')
+    .format('MM-DD-YYYY')
+)
+  end_date = moment()
+    .add(6, 'months')
+    .subtract(3, 'd');
+
+console.log(end_date.format('MM-DD-YYYY'));
+
+/////////////////// Настройка направлений //////////////////////////
+const departure = 'riga-coach-station';
+const destination = 'vilnius-coach-station';
 
 const dataFromPageCollection = async date => {
-  const { data: html } = await axios.get(
-    `${base_url}/poezdku-raspisanija/riga-coach-station/vilnius-coach-station?Date=${date}&Currency=CURRENCY.EUR`
-  );
+  const { data: html } = await axios.get(`${base_url}/poezdku-raspisanija/${departure}/${destination}?Date=${date}`);
   const allRoutes = html.match(/TripId&(.*?)}/g);
+  if (!allRoutes) return;
   allRoutes.forEach(route => {
     addingToRotesFromAllPagesArray(route.match(/\d+/g), date);
   });
@@ -40,9 +52,6 @@ const addingToRotesFromAllPagesArray = (route, date) => {
 
 const addingToQueryArray = () => {
   for (let i = 0; i < routesFromAllPages.length / legsPerQuery; i++) {
-    console.log(
-      `From i * legsPerQuery ${i * legsPerQuery} to legsPerQuery * i + legsPerQuery ${legsPerQuery * i + legsPerQuery}`
-    );
     allQueries.push(routesFromAllPages.slice(i * legsPerQuery, legsPerQuery * i + legsPerQuery));
   }
 };
@@ -50,29 +59,27 @@ const addingToQueryArray = () => {
 const priceCalculation = async query => {
   const {
     data: { Trips },
-  } = await axios.post(`${base_url}/TripBonusCalculator/CalculateSpecialPrice`, query);
+  } = await axios.post(`${base_url}/TripBonusCalculator/CalculateSpecialPrice`, [{ Legs: query }]);
 
-  Trips.forEach((el, i) => {
-    el.IsSpecialPrice ? console.log(`Date: ${query[0].Legs[i].date}. Price: ${el.Price}. TripID: ${el.TripId}`) : null;
+  Trips.forEach(({ IsSpecialPrice, Price, TripId }, i) => {
+    IsSpecialPrice ? console.log(`Date: ${query[i].date}. Price: ${Price}. TripID: ${TripId}`) : null;
   });
 };
 
 const dates = [];
 for (let m = start_date; m.diff(end_date, 'd') <= 0; m.add(1, 'd')) dates.push(m.format('MM-DD-YYYY'));
-Promise.map(dates, date => dataFromPageCollection(date), { concurrency })
-  .then(() => {
-    addingToQueryArray();
-    Promise.map(
-      allQueries,
-      el => {
-        query[0].Legs = el;
-        return priceCalculation([{ Legs: el }]);
-      },
-      { concurrency }
-    ).catch(e => console.log('Проблемы во втором промисе------------------------------', e));
-  })
-  .catch(e => console.log('Проблемы в первом промисе------------------------------', e));
+// Promise.map(dates, date => dataFromPageCollection(date), { concurrency })
+//   .then(() => {
+//     addingToQueryArray();
+//     Promise.map(allQueries, query => priceCalculation(query), { concurrency }).catch(e =>
+//       console.log('Проблемы во втором промисе------------------------------', e)
+//     );
+//   })
+//   .catch(e => console.log('Проблемы в первом промисе------------------------------', e));
 
 // Решить проблему с несуществующими рейсами в конкретную дату
 // allQueries не глобальная переменная
-//6165523
+// Упростить regex
+// Научится определять сумму со всеми пересадками, и ограничивать по цене
+// Научится определять максимальное кол-во legs и concurrency, и умно их выставлять
+// https://ticket.luxexpress.eu/ru/Stops/FindBy?stopName=riga - Запрос остановки --> slug - элементы ссылки
