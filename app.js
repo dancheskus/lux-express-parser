90a8cb20d74a498d1bb93bfabd632d44b7e6e32a
DESKTOP-89N3UKL\danch <dancheskus@gmail.com>
2018-07-19 10:30:38 +0300

    allQueries variable is not local now

--- app.js
+++ app.js
@@ -4,15 +4,14 @@ const moment = require('moment');
 
 const base_url = 'https://ticket.luxexpress.eu/ru';
 const routesFromAllPages = [];
-const allQueries = [];
 
 /////////////////// Запросы на сервер //////////////////////////
-const concurrency = 5;
-const legsPerQuery = 50;
+let concurrency = 5;
+let legsPerQuery = 50;
 
 /////////////////// Настройка дат //////////////////////////
-const start_date = moment().add(1, 'd');
-let end_date = moment().add(6, 'M');
+const start_date = moment();
+let end_date = moment().add(6, 'd');
 
 if (
   // Ограничиваем 6-ю месяцами
@@ -30,22 +29,23 @@ const departure = 'riga-coach-station';
 // const destination = 'minsk-central-coach-station';
 const destination = 'vilnius-coach-station';
 
+/////////////////// Парсинг HTML страницы //////////////////////////
 const dataFromPageCollection = async date => {
   const { data: html } = await axios.get(`${base_url}/poezdku-raspisanija/${departure}/${destination}?Date=${date}`);
   const allRoutes = html.match(/TripId&(.*?)}/g);
   if (!allRoutes) return;
   allRoutes.forEach(el => {
-    const match = el.match(/\d+/g);
+    const [TripId, DepartureRouteStopId, DestinationRouteStopId] = el.match(/\d+/g);
     routesFromAllPages.push({
-      TripId: match[0],
-      DepartureRouteStopId: match[1],
-      DestinationRouteStopId: match[2],
+      TripId,
+      DepartureRouteStopId,
+      DestinationRouteStopId,
       date,
     });
   });
 };
 
-const addingToQueryArray = () => {
+const addingToQueryArray = allQueries => {
   for (let i = 0; i < routesFromAllPages.length / legsPerQuery; i++) {
     allQueries.push(routesFromAllPages.slice(i * legsPerQuery, legsPerQuery * i + legsPerQuery));
   }
@@ -65,7 +65,8 @@ const dates = [];
 for (let m = start_date; m.diff(end_date, 'd') <= 0; m.add(1, 'd')) dates.push(m.format('MM-DD-YYYY'));
 Promise.map(dates, date => dataFromPageCollection(date), { concurrency })
   .then(() => {
-    addingToQueryArray();
+    const allQueries = [];
+    addingToQueryArray(allQueries);
     Promise.map(allQueries, query => priceCalculation(query), { concurrency }).catch(e =>
       console.log('Проблемы во втором промисе------------------------------', e)
     );
@@ -73,7 +74,6 @@ Promise.map(dates, date => dataFromPageCollection(date), { concurrency })
   .catch(e => console.log('Проблемы в первом промисе------------------------------', e));
 
 // allQueries не глобальная переменная
-// Упростить regex
 // Научится определять сумму со всеми пересадками, и ограничивать по цене
 // Научится определять максимальное кол-во legs и concurrency, и умно их выставлять
 // https://ticket.luxexpress.eu/ru/Stops/FindBy?stopName=riga - Запрос остановки --> slug - элементы ссылки
