87822ae07de5262b8bb4d9173854e82b5c3ed170
DESKTOP-89N3UKL\danch <dancheskus@gmail.com>
2018-09-13 13:14:36 +0300

    promiseOptions --> async/await

--- frontend/src/components/SearchStop.js
+++ frontend/src/components/SearchStop.js
@@ -1,35 +1,3 @@
-// import React from 'react';
-// import Select from 'react-select';
-// import Async from 'react-select/lib/Async';
-
-// const options = [
-//   { value: 'chocolate', label: 'Chocolate' },
-//   { value: 'strawberry', label: 'Strawberry' },
-//   { value: 'vanilla', label: 'Vanilla' },
-// ];
-
-// export default class SearchStop extends React.Component {
-//   state = {
-//     selectedOption: null,
-//   };
-//   handleChange = selectedOption => {
-//     this.setState({ selectedOption });
-//     console.log(`Option selected:`, selectedOption);
-//   };
-//   render() {
-//     const { selectedOption } = this.state;
-
-//     return (
-//       <Select
-//         value={selectedOption}
-//         onChange={this.handleChange}
-//         placeholder={'Начальная остановка'}
-//         options={options}
-//       />
-//     );
-//   }
-// }
-
 import React, { Component } from 'react';
 
 import axios from 'axios';
@@ -40,19 +8,21 @@ export default class WithPromises extends Component {
 
   onChange = selectedStop => this.props.onStopChange(this.props.direction, selectedStop);
 
-  promiseOptions = () =>
-    axios
-      .get(
+  promiseOptions = async () => {
+    try {
+      const allStopList = await axios.get(
         `https://cors-anywhere.herokuapp.com/https://ticket.luxexpress.eu/ru/Stops/FindBy?stopName=${
           this.state.inputValue
         }`
-      )
-      .then(result =>
-        result.data.map(stop => {
-          return { value: stop.Slug, label: stop.StopName };
-        })
-      )
-      .catch(e => console.log(e));
+      );
+
+      return allStopList.data.map(stop => {
+        return { value: stop.Slug, label: stop.StopName };
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   handleInputChange = inputValue => this.setState({ inputValue });
 
