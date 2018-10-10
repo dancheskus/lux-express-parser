9b718834ebd1e520d3e47ea87171538c2dd9235f
DESKTOP-89N3UKL\danch <dancheskus@gmail.com>
2018-10-10 11:59:21 +0300

    Old mongoose folder removed

--- backend/mongoose/mongooseConfig.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect(
-  'mongodb+srv://dancheskus:12345@cluster0-ruylq.mongodb.net/LuxExpress',
-  { useNewUrlParser: true }
-);
-mongoose.set('useCreateIndex', true);
