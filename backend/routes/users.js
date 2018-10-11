01739243873978e3759032edaacb1f932062ffee
DESKTOP-89N3UKL\danch <dancheskus@gmail.com>
2018-10-11 16:54:57 +0300

    Добавлена проверка токена

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,7 +1,17 @@
 const User = require('../DB/models/user');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const router = require('express').Router();
+
+const isVerified = async (req, res, next) => {
+  try {
+    req.user = await User.findOne({ _id: jwt.verify(req.headers['x-auth'], process.env.SECRET_KEY).id });
+    next();
+  } catch (error) {
+    res.status(400).json({ message: 'Please log in' });
+  }
+};
 
 router.post('/register', async (req, res) => {
   try {
@@ -15,10 +25,19 @@ router.post('/register', async (req, res) => {
 router.post('/login', async (req, res) => {
   try {
     const user = await User.findOne({ username: req.body.username });
-    const decoded = bcrypt.compareSync(req.body.password, user.password);
-    res.json(decoded);
+    if (!bcrypt.compareSync(req.body.password, user.password)) return;
+    const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY); //, { expiresIn: '10s' }
+    res.json({ user, token });
+  } catch (e) {
+    res.status(400).json({ message: 'Not authorized' });
+  }
+});
+
+router.get('/test', isVerified, (req, res) => {
+  try {
+    res.json(req.user);
   } catch (error) {
-    res.status(400).json({ error });
+    res.status(400).json({ message: 'please log in' });
   }
 });
 
