1c2f424fc02b289b559a7d99237d76a17b361d25
DESKTOP-89N3UKL\danch <dancheskus@gmail.com>
2018-10-20 18:35:35 +0300

    Revert "Removes unneccessary Route componnet"
    This reverts commit 7740e508a93970fa9720fed631006f0019a17beb.

--- frontend/src/PrivateRoute.js
+++ frontend/src/PrivateRoute.js
@@ -2,11 +2,17 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
 
-export const PrivateRoute = ({ component: Component, ...props }) =>
-  true ? (
-    <Component {...props} />
-  ) : (
-    <div>
-      <h1>Is NOT Logged IN</h1>
-    </div>
-  );
+export const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    component={props =>
+      true ? (
+        <Component {...props} />
+      ) : (
+        <div>
+          <h1>Is NOT Logged IN</h1>
+        </div>
+      )
+    }
+  />
+);
